d0ff03e54dfc8608d3520615548af070938b54c8
Waleedanwar01 <waleeddogare@gmail.com>
2025-08-29 16:02:51 +0500

    Fix Vercel deployment issues

--- postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-import postcssPlugin from "@tailwindcss/postcss";
-
-const config = {
-  plugins: [tailwindcss, autoprefixer, postcssPlugin],
-};
-
-export default config;
