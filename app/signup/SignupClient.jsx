ccfce4f57130c2a95f119cce8821354e576aa001
Waleedanwar01 <waleeddogare@gmail.com>
2025-08-30 11:19:42 +0500

    new

--- app/signup/SignupClient.jsx
+++ app/signup/SignupClient.jsx
@@ -13,7 +13,7 @@ export default function SignupPage() {
   async function handleSignup(e) {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:8000/auth/signup/", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/signup/`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password }),
